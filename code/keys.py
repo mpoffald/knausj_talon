f22bee742be4d50834ab04751a61d55d594bb223
Marcela Poffald <mpoffald@gmail.com>
2021-04-13 17:00:05 -0400

    ship=shift

--- code/keys.py
+++ code/keys.py
@@ -115,7 +115,7 @@ modifier_keys = {
     # If you find 'alt' is often misrecognized, try using 'alter'.
     "alt": "alt",  #'alter': 'alt',
     "control": "ctrl",  #'troll':   'ctrl',
-    "shift": "shift",  #'sky':     'shift',
+    "ship": "shift",  #'sky':     'shift',
     "super": "super",
 }
 if app.platform  == "mac":
